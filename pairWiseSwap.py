b5eaaa12daed76a357dcdf1549c6ac2cdd13fa4a
Aswanth Anoop <77261421+aswanthanoop@users.noreply.github.com>
2023-06-22 19:16:12 +0530

    Add files via upload

---
+++ pairWiseSwap.py
@@ -0,0 +1,60 @@
+class Node:
+    def __init__(self, val = 0,nxt = None):
+        self.val = val
+        self.next = nxt
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+    def append(self, val):
+        new_node = Node(val = val)
+        if self.head:
+            self.tail.next = new_node
+            self.tail = self.tail.next
+        else:
+            self.head = new_node
+            self.tail = new_node
+    def show(self):
+        temp = self.head
+        while temp:
+            print(temp.val, end=" -> ")
+            temp = temp.next
+        print("None")
+
+
+    def remove_the_nth_element(self,n):
+        temp = self.head
+        if self.length == 0 or self.length >n:
+            print("UnderFlow")
+        elif n == 0:
+            self.delete_end()
+        elif n ==self.length:
+            self.delete_start()
+        else:
+            for i in range(self, length - n - 1 ):
+                temp = temp.next
+            temp.next = temp.next.next
+        self.length -= 1
+
+    def pairwise_swap(self):
+        l = self.head
+        r = self.head.next
+        l.next = r.next
+        r.next = l
+        self.head = r
+        temp = l
+        l = l.next
+        r = l.next
+        while l and r:
+            temp.next = r
+            l.next = r.next
+            r.next = l
+            temp = l
+            if l.next and r.next:
+                l = l.next
+                r = l.next
+            else:
+                break    
+            
