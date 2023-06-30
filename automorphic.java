91697076f3c85bb2f54797ad2790fb83a33ae131
aswanthanoop <aswanthanoop42@gmail.com>
2023-06-30 18:56:49 +0530

    updated changes

--- automorphic.java
+++ automorphic.java
@@ -1,12 +1,18 @@
 public class automorphic {
-    int n;
-    int sqr = n*n;
+    public static boolean isAutomorphic(int n) {
+        int sqr = n * n;
+        int powerOf10 = (int) Math.pow(10, String.valueOf(n).length());
 
-    if(sqr%(10**void len(str(n)))==n){
-        return true;
-    else{
-        return false;
+        if (sqr % powerOf10 == n) {
+            return true;
+        } else {
+            return false;
         }
     }
-    
+
+    public static void main(String[] args) {
+        int n = 25;
+        boolean isAutomorphic = isAutomorphic(n);
+        System.out.println("Is Automorphic: " + isAutomorphic);
+    }
 }
