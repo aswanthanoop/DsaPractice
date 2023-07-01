5501a05fe1fe4703ba190cebabc730e6290a1176
aswanthanoop <aswanthanoop42@gmail.com>
2023-07-01 17:35:09 +0530

    Updated Changes

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -1,8 +1,11 @@
 import java.util.*;
-public class LinkedList{
+
+\\\
+
+public class LinkedList {
     Node head;
 
-    static class Node{
+    static class Node {
         int data;
         Node next;
 
@@ -12,34 +15,41 @@ public class LinkedList{
         }
     }
 
-    public static LinkedList insert(LinkedList list, int data){
+    public static LinkedList insert(LinkedList list, int data) {
         Node new_node = new Node(data);
 
-        if(list.head ==null){
+        if (list.head == null) {
             list.head = new_node;
-        }
-        else {
+        } else {
             Node last = list.head;
-            while (last.next != null){
+            while (last.next != null) {
                 last = last.next;
             }
             last.next = new_node;
         }
 
         return list;
-
     }
 
-    public static void printList(LinkedList list){
+    public static void printList(LinkedList list) {
         Node currNode = list.head;
         System.out.println("LinkedList: ");
 
-    
-        while (currNode != null){
-            System.out.print(currNode.data)
+        while (currNode != null) {
+            System.out.print(currNode.data + " ");
+            currNode = currNode.next;
         }
     }
 
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
 
+        list = insert(list, 1);
+        list = insert(list, 2);
+        list = insert(list, 3);
+        list = insert(list, 4);
+        list = insert(list, 5);
 
+        printList(list);
+    }
 }
--- ses2/Linkedlist.java
+++ ses2/Linkedlist.java
@@ -64,6 +64,7 @@ public class Linkedlist {
             System.out.println();
         }
     }
+    
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
         list.insertAtBeginning(1);
